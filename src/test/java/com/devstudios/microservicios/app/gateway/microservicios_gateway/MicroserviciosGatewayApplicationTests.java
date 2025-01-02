042513e69a6b31e8462a6b8b2cdb5b5fbbf51693
DevCompleteStudios <jorge.yael333@gmail.com>
2025-01-02 08:30:05 -0600

    Agregando cliente de eureka y gateway

---
+++ src/test/java/com/devstudios/microservicios/app/gateway/microservicios_gateway/MicroserviciosGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstudios.microservicios.app.gateway.microservicios_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
